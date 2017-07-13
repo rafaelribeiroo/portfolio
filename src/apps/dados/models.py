d5a2dfad0ae0af38d30b4640c3203af50d3b6e48
Rafael1994 <pereiraribeirorafael@gmail.com>
2017-07-13 15:30:56 -0300

    Consertado e slice no template

--- src/apps/dados/models.py
+++ src/apps/dados/models.py
@@ -3,7 +3,7 @@ from .data import LISTA_MESES #Combobox
 
 class DadoPessoal(models.Model):
     title = models.CharField(max_length=40, verbose_name='Título')
-    description = models.TextField(max_length=100, verbose_name='Descrição')
+    description = models.TextField(max_length=500, verbose_name='Descrição')
     dia_evento = models.CharField(max_length=2, verbose_name='Dia da Ocorrência')
     mes_evento = models.PositiveSmallIntegerField(choices=LISTA_MESES, default='Janeiro', verbose_name='Mês da Ocorrência')
     ano_evento = models.CharField(max_length=4, verbose_name='Ano da Ocorrência')
