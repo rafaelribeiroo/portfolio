b21512426eb3abd64e3be8a88018ea49b99e5879
Rafael1994 <pereiraribeirorafael@gmail.com>
2017-07-12 23:50:34 -0300

    Modelos criado

---
+++ src/apps/dados/data.py
@@ -0,0 +1,26 @@
+JAN = 1
+FEV = 2
+MAR = 3
+ABR = 4
+MAI = 5
+JUN = 6
+JUL = 7
+AGO = 8
+SET = 9
+OUT = 10
+NOV = 11
+DEZ = 12
+LISTA_MESES = (
+    (JAN, 'Janeiro'),
+    (FEV, 'Fevereiro'),
+    (MAR, 'Março'),
+    (ABR, 'Abril'),
+    (MAI, 'Maio'),
+    (JUN, 'Junho'),
+    (JUL, 'Julho'),
+    (AGO, 'Agosto'),
+    (SET, 'Setembro'),
+    (OUT, 'Outubro'),
+    (NOV, 'Novembro'),
+    (DEZ, 'Dezembro'),
+)
--- src/apps/dados/models.py
+++ src/apps/dados/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from .data import LISTA_MESES #Combobox
 
-# Create your models here.
+class DadoPessoal(models.Model):
+    title = models.CharField(max_length=40, verbose_name='Título')
+    description = models.TextField(max_length=100, verbose_name='Descrição')
+    dia_evento = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Dia da Ocorrência')
+    mes_evento = models.PositiveSmallIntegerField(choices=LISTA_MESES, default='Janeiro')
+    ano_evento = models.CharField(max_length=3, verbose_name='Ano da Ocorrência')
+
+    def __str__(self):
+        return self.title
