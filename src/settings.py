d646de251bea9c1f7b55a849c3a295c0aed98fc9
Rafael1994 <pereiraribeirorafael@gmail.com>
2017-07-12 21:19:26 -0300

    App Dados

--- src/settings.py
+++ src/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #3rd. apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'src.apps.dados',
 ]
 
 MIDDLEWARE = [
