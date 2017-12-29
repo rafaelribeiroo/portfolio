d5285dba014eeaec190e5922bcb12f870aad4ad7
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-12-28 23:50:25 -0200

    1.11 to 2.0

--- src/apps/dados/urls.py
+++ src/apps/dados/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from .views import home
 
+
+app_name = 'dados'
 urlpatterns = [
-    url(r'^$', home),
+    path('', home, name='homepage'),
 ]
