54cc41bc16415a5331fa3bc3f3745257649308c3
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-01-03 22:57:43 -0200

    Referring variable

--- src/wsgi.py
+++ src/wsgi.py
@@ -18,4 +18,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings.production")
 
 # application = get_wsgi_application()
 application = Cling(get_wsgi_application())
-application = DjangoWhiteNoise(application())
+application = DjangoWhiteNoise(application)
