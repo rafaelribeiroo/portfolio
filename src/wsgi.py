58c5d9a039399e52aa7c546274af054e51a99547
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-01-03 22:36:51 -0200

    Now go?!

--- src/wsgi.py
+++ src/wsgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
+# from dj_static import Cling
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings.production")
 
 # application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
