e75b48887440e1b4e8f91d1b201002d54f63d298
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-01-03 23:18:41 -0200

    Modifying front

---
+++ src/staticfiles/js/scrollspy.2fc1407f19ba.js
@@ -0,0 +1,69 @@
+class StickyNavigation {
+
+	constructor() {
+		this.currentId = null;
+		this.currentTab = null;
+		this.tabContainerHeight = 70;
+		let self = this;
+		$('.scrollspy').click(function() {
+			self.onTabClick(event, $(this));
+		});
+		$(window).scroll(() => { this.onScroll(); });
+		$(window).resize(() => { this.onResize(); });
+	}
+
+	onTabClick(event, element) {
+		event.preventDefault();
+		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+		$('html, body').animate({ scrollTop: scrollTop }, 600);
+	}
+
+	onScroll() {
+		this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+	}
+
+	onResize() {
+		if(this.currentId) {
+			this.setSliderCss();
+		}
+	}
+
+	checkTabContainerPosition() {
+		let offset = $('.tabs').offset().top + $('.tabs').height() - this.tabContainerHeight;
+	}
+
+	findCurrentTabSelector(element) {
+		let newCurrentId;
+		let newCurrentTab;
+		let self = this;
+		$('.scrollspy').each(function() {
+			let id = $(this).attr('href');
+			let offsetTop = $(id).offset().top - self.tabContainerHeight;
+			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+				newCurrentId = id;
+				newCurrentTab = $(this);
+			}
+		});
+		if(this.currentId != newCurrentId || this.currentId === null) {
+			this.currentId = newCurrentId;
+			this.currentTab = newCurrentTab;
+			this.setSliderCss();
+		}
+	}
+
+	setSliderCss() {
+		let width = 0;
+		let left = 0;
+		if(this.currentTab) {
+			width = this.currentTab.css('width');
+			left = this.currentTab.offset().left;
+		}
+		$('.tab-slider').css('width', width);
+		$('.tab-slider').css('left', left);
+	}
+
+}
+
+new StickyNavigation();
