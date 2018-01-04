d2c75ceaad63c5d6c4d6dcf35a50173291f3db52
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-01-03 22:48:11 -0200

    Cling

--- src/wsgi.py
+++ src/wsgi.py
@@ -11,11 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-# from dj_static import Cling
+from dj_static import Cling
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings.production")
 
 # application = get_wsgi_application()
-#application = Cling(get_wsgi_application())
-application = DjangoWhiteNoise(get_wsgi_application())
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application())
